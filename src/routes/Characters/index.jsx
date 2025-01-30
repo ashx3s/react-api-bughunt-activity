83a07ef4bc95779355fae9d5ea5a4e56cff0d956
ashx3s <ashlyn@lilyx.io>
2025-01-30 14:09:13 -0700

    simple styles for character page

--- src/routes/Characters/index.jsx
+++ src/routes/Characters/index.jsx
@@ -1,22 +1,44 @@
+import { useState, useEffect } from "react";
 import PageHeader from "../../components/PageHeader";
 
-const apiUrl = "https://api.disneyapi.dev/character";
-
-async function getCharacters(url = apiUrl) {
-  const response = await fetch(url);
-  const characters = await response.json();
-  console.log(characters);
-}
-
-getCharacters();
 export default function Characters() {
+  // state for characters
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    async function getCharacters() {
+      const apiUrl = "https://api.disneyapi.dev/character";
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error(`HTTP error status: ${response.status}`);
+        }
+        const characters = await response.json();
+        setCharacters(characters.data);
+      } catch (err) {
+        console.error("Error fetching characters", err);
+      }
+    }
+    getCharacters();
+  }, []);
+  console.log(characters);
   return (
     <main>
       <PageHeader
         title="Characters"
         description="these are some disney characters"
       />
-      <div>{/* Render characters here */}</div>
+      <div>
+        <ul>
+          {characters.map((character) => {
+            return (
+              <li key={character._id} className="my-6">
+                <h3 className="mb-2 text-2xl">{character.name}</h3>
+                <img src={character.imageUrl} alt={character.name} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </main>
   );
 }
